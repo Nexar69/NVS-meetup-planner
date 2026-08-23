cfb3c835b67d435529942ad427561ea1ffc2744a
Nexar <96820799+Nexar69@users.noreply.github.com>
2026-08-23 18:18:39 +0200

    Bump PWA cache for v0.5.2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meet-schwerin-v0.5.1-r1";
+const CACHE_NAME = "meet-schwerin-v0.5.2-r1";
 
 const APP_SHELL = [
   "./",
@@ -9,13 +9,16 @@ const APP_SHELL = [
   "./places.css",
   "./fair.css",
   "./journey.css",
+  "./recommend.css",
   "./ux-v051.css",
   "./transit.js",
   "./places.js",
+  "./recommend.js",
   "./map.js",
   "./v05.js",
   "./ux-v051.js",
   "./app.js",
+  "./results-v052.js",
   "./fair.js",
   "./journey.js",
   "./manifest.webmanifest",
