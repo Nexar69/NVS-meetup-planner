0b8fc3a03f226bea5cacaae49392a6637673f8f2
Nexar <96820799+Nexar69@users.noreply.github.com>
2026-08-24 06:14:56 +0200

    Activate Meet Schwerin Cloudflare backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 (() => {
   const injectedBackend = String(window.__NVS_BACKEND_URL__ || "").replace(/\/+$/, "");
-  const configuredBackend = ""; // Filled after the Cloudflare Worker is deployed.
+  const configuredBackend = "https://meet-schwerin.timothy-ua-pa.workers.dev";
   const backendUrl = injectedBackend || configuredBackend;
 
   window.NVSConfig = Object.freeze({
